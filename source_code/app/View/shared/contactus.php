0aab18040cb1f3691add4b29c4b877e5eba4335e
Aleksy Ruszala <16pxdesign@gmail.com>
2020-03-11 11:45:59 +0000

    Add contact us to source code

---
+++ source_code/app/View/shared/contactus.php
@@ -0,0 +1,25 @@
+<!--
+ * Contact
+ * @author     Lewis Mckaig
+-->
+<div class="container">
+
+    <form class="p-3" action="" method="post">
+
+        <div class="form-group col-sm-8 offset-sm-2">
+            <label for="email">Your Email:</label>
+            <input id="email" type="email" name="email" class="form-control rounded" value="" required>
+            <span class="help-block"></span>
+        </div>
+        <div class="form-group col-sm-8 offset-sm-2">
+            <label for="message" >Your Message:</label>
+            <textarea id="message" name = "message" rows="4" cols="50" value = "" class="form-control rounded"></textarea>
+            <span class="help-block"></span>
+        </div>
+        <div class="form-group col-sm-8 offset-sm-2">
+            <input type="submit" class="btn btn-primary" value="Submit" style ="background-color:#003881">
+        </div>
+
+    </form>
+
+</div>
