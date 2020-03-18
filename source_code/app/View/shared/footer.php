61e3c5fe81aa015c1c688ef774e3f6360377547e
Aleksy Ruszala <16pxdesign@gmail.com>
2020-03-18 10:22:59 +0000

    Change footer to fixed position

--- source_code/app/View/shared/footer.php
+++ source_code/app/View/shared/footer.php
@@ -6,48 +6,43 @@
 
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" href="/~cmp311gc1904/css/sticky_footer.css">
 
 <style>
-    .footer {
-        position: absolute;
-        bottom: 0;
+    footer {
+        position: fixed;
         background: #002147;
         height: 60px;
-        padding-bottom: 30px;
         width: 100%;
-        border-bottom: 1px solid #CCCCCC;
-        border-top: 1px solid #DDDDDD;
+        box-sizing: content-box;
     }
 
-    .footer img {
+    footer img {
         max-width: 100%;
     }
 
-    .footer ul li a {
+    footer ul li a {
         padding: 0 0 12px 0;
         display: block;
     }
-    .footer a {
+    footer a {
         color: white;
         font-weight: lighter;
     }
-    .footer a i  {
+    footer a i  {
         font-size: 3em;
     }
     .fa-10x{
-        font-size: 4em;
+        font-size: 3.5em;
     }
 </style>
 
 
 <footer>
-
-    <div class="footer" id="footer">
-        <div class="container d-flex justify-content-center mt-1">
+        <div class="container d-flex justify-content-center align-items-center ">
             <a  href="http://google.com"><i class="material-icons p-1">question_answer</i></a>
             <a  href="http://google.com"><i class="material-icons p-1">camera_alt</i></a>
             <a  href="http://google.com"><i class="fa fa-qrcode fa-10x p-1"></i></a>
             <a  href="http://google.com"><i class="material-icons p-1">email</i></a>
         </div>
-    </div>
 </footer>
